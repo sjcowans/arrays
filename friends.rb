47aa52ba495de2a29e1049af444378018f155028
Sean Cowans <zedesia@yahoo.com>
2023-01-04 13:21:45 -0700

    Add an array with methods

--- friends.rb
+++ friends.rb
@@ -2,3 +2,10 @@ arrays = ["Friend 1","Friend 2","Friend 3","Friend 4","Friend 5"]
 puts arrays[2]
 puts arrays[4]
 puts arrays[0]
+vacation = ["Earth","El Paso","Moon","Dallas","Colorado"]
+vacation.pop
+puts vacation
+vacation.push("Colorado")
+puts vacation
+vacation.push("Food")
+puts vacation
